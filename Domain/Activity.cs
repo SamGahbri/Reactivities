0925bd9f2c97d07a3cd608ea01acabcaf1c11f1e
sarun <sarun@DESKTOP-UTECNVI>
2026-02-12 05:27:57 +0000

    Add project files.

---
+++ Domain/Activity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public required string PatientName { get; set; } //user (required) to make fields nullable
+
+        public required string TreatmentName { get; set; } 
+
+        public DateTime TreatmentDate { get; set; }
+
+        public required string TreatmentDescription { get; set; }
+
+        public required string TreatmentCategory { get; set; }
+
+        public bool IsApproved { get; set; }
+
+
+        //locations props
+
+        public required string City { get; set; }
+
+        public required string DenturePractice { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
---
+++ Persistence/DbInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Domain;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DbInitializer
+    {
+    }
+}
